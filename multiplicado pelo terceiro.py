c4d671c2c58236c7a75830b65bbc724d67ee02e3
TiagoLopes-prog <tlteles046@gmail.com>
2024-09-24 17:09:53 -0400

    Add files via upload

---
+++ multiplicado pelo terceiro.py
@@ -0,0 +1,7 @@
+num1 =  float(input("Primeiro numero: "))
+num2 = float(input("Segundo numero: "))
+num3 =  float(input("Terceiro numero: "))
+
+total = float((num1+num2)*num3)
+
+print("Total: ",total)
