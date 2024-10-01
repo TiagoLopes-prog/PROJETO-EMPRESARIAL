1bdd86be4426a8a0f25973b7e29492331571c94b
TiagoLopes-prog <tllopes046@gmail.com>
2024-10-01 17:31:20 -0400

    Vetores

---
+++ lista.py
@@ -0,0 +1,2 @@
+list = [1,2,1,5,6,4,3,1]
+print(list.count (1))
---
+++ vetores.py
@@ -0,0 +1,6 @@
+numeros = [17,18,19,20]
+print(numeros)
+print(max(numeros))
+print(min(numeros))
+print(len(numeros))
+print(sum(numeros))
